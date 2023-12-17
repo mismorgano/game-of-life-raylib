256409c7d32afec6ceada1f8588a62100a51f20f
mismorgano <mismorgano@gmail.com>
2023-12-17 17:17:23 -0600

    Create grid

---
+++ main.cpp
@@ -0,0 +1,99 @@
+//
+// Created by Anton on 03/12/2023.
+//
+
+#include "raylib.h"
+#include "raymath.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+
+
+int main() {
+    int rows = 20;
+    int columns = 30;
+
+
+    auto cellSize = 10;
+    // Initialization
+
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = cellSize * columns;
+    const int screenHeight = cellSize * rows;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - 2d camera mouse zoom");
+
+    Camera2D camera = {0};
+    camera.zoom = 1.0f;
+
+    SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    vector<pair<int, int>> cellsAlives{make_pair(10, 10)};
+    // Main game loop
+    while (!WindowShouldClose())        // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        // Translate based on mouse right click
+        if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
+            Vector2 delta = GetMouseDelta();
+            delta = Vector2Scale(delta, -1.0f / camera.zoom);
+
+            camera.target = Vector2Add(camera.target, delta);
+        }
+
+
+        // Zoom based on mouse wheel
+        float wheel = GetMouseWheelMove();
+        if (wheel != 0) {
+            // Get the world point that is under the mouse
+            Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
+
+            // Set the offset to where the mouse is
+            camera.offset = GetMousePosition();
+
+            // Set the target to match, so that the camera maps the world space point
+            // under the cursor to the screen space point under the cursor at any zoom
+            camera.target = mouseWorldPos;
+
+            // Zoom increment
+            const float zoomIncrement = 0.925f;
+
+            camera.zoom += (wheel * zoomIncrement);
+            if (camera.zoom < zoomIncrement) camera.zoom = zoomIncrement;
+        }
+
+        //----------------------------------------------------------------------------------
+        int slices = 100;
+        int spacing = cellSize;
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        BeginMode2D(camera);
+        for (int i = 0; i < columns; ++i) {
+            DrawLine(spacing * i, 0, spacing * i, screenHeight, BEIGE);
+        }
+        for (int i = 0; i < rows; ++i) {
+            DrawLine(0, spacing*i, screenWidth, spacing*i, BEIGE);
+        }
+
+        EndMode2D();
+//        GuiButton((Rectangle){ 0, 0, 125, 30 }, GuiIconText(ICON_FILE_SAVE, "Save File"));
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+    return 0;
+}
